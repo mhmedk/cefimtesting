58d8be1d8eb5072205597d338833eea17288006f
Mohammed K <mohammed.kasbi45@gmail.com>
2023-05-23 15:20:09 +0200

    feat: Ajout du back --DOING

--- front/app.js
+++ front/app.js
@@ -1,21 +1,37 @@
-// Données des apprenants filtrés pour le dev
-let apprenantsFalse = [
-    [
-        { promotion: 2021, nom: "Apprenant 1", prenom: "John", age: 25, sexe: "Homme", competences: ["JavaScript", "HTML"] },
-        { promotion: 2021, nom: "Apprenant 1", prenom: "Marc", age: 25, sexe: "Homme", competences: ["JavaScript", "HTML"] },
-    ],
-    [
-        { promotion: 2021, nom: "Apprenant 1", prenom: "Louis", age: 25, sexe: "Homme", competences: ["JavaScript", "HTML"] },
-        { promotion: 2021, nom: "Apprenant 1", prenom: "Mohammed", age: 25, sexe: "Homme", competences: ["JavaScript", "HTML"] },
-    ],
-    [
-        { promotion: 2021, nom: "Apprenant 1", prenom: "Bilal", age: 25, sexe: "Homme", competences: ["JavaScript", "HTML"] },
-        { promotion: 2021, nom: "Apprenant 1", prenom: "Pierre", age: 25, sexe: "Homme", competences: ["JavaScript", "HTML"] },
-    ]
-]
+// Ajout du listener au chargement du DOM
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('groupForm')
+    form.addEventListener('submit', getApprenants)
+})
 
-// Fonction d'affichage des apprenants filtrés
-function afficherApprenants(apprenantsFiltres = apprenantsFalse) {
+// Fonction pour récupérer les apprenants depuis le serveur
+function getApprenants(event) {
+
+    event.preventDefault()
+
+    const maxApprenantsPerGroup = document.getElementById('groupSize').value
+
+    // Faire une requête fetch pour obtenir les apprenants
+    fetch('http://localhost:8080/api.php?maxApprenantsPerGroup=' + maxApprenantsPerGroup)
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('Erreur lors de la récupération des apprenants : ' + response.status)
+            }
+        })
+        .then((groups) => {
+            displayGroups(groups)
+        })
+        .catch(function(error) {
+            console.error('Erreur lors de la récupération des apprenants : ' + error.message)
+        })
+
+}
+
+// Fonction d'affichage des apprenants
+function displayGroups(apprenantsFiltres) {
+
     const result = document.getElementById("resultats")
     result.innerHTML = ""
 
