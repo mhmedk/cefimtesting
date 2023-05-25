261dc50251039fbd32781196cd6ac36633449c63
MohammedK <mohammed.kasbi45@gmail.com>
2023-05-25 17:33:39 +0200

    fix: filtre par compétences --DONE

--- back/group.php
+++ back/group.php
@@ -12,15 +12,17 @@ $apprenants = selectLearner($bdd);
  * @param boolean $genderFilter Savoir si le filtre es actif.
  * @return array Les groupes formés avec les apprenants répartis avec le filtre.
  */
-function createGroups($apprenants, $groupSize, $genderFilter) {
+function createGroups($apprenants, $groupSize, $genderFilter, $skillsFilter) {
     // Mélanger les apprenants aléatoirement
     shuffle($apprenants);
     $groupSize = intval($groupSize);
 
     
-    if($genderFilter){
+    if ($genderFilter) {
         return genderFilter($apprenants, $groupSize);
-    }else{
+    } else if ($skillsFilter) {
+        return skillsFilter($apprenants, $groupSize);
+    } else {
         return groupsNoFilter($apprenants, $groupSize);
     }
     
@@ -95,11 +97,59 @@ function genderFilter($apprenants, $groupSize) {
     return $mixedGroup;
 }
 
+/**
+ * Fonction pour créer des groupes mixtes en répartissant les apprenants par compétences.
+ *
+ * @param array $apprenants Les données des apprenants sous forme de tableau associatif.
+ * @param int $groupSize Le nombre d'apprenants par groupe.
+ * @return array Les groupes mixtes formés avec les apprenants répartis par genre.
+ */
+function skillsFilter($apprenants, $groupSize) {
+    // Trier les apprenants par compétences
+    $apprenantsBack = array_filter($apprenants, function($apprenant) {
+        return $apprenant['skills'] === 'PHP' || $apprenant['skills'] === 'JAVA';
+    });
+    $apprenantsFront = array_filter($apprenants, function($apprenant) {
+        return $apprenant['skills'] === 'HTML' || $apprenant['skills'] === 'CSS';
+    });
 
+    // Mélanger les apprenants de chaque groupe
+    shuffle($apprenantsBack);
+    shuffle($apprenantsFront);
+
+    // Créer les groupes mixtes
+    $mixedGroup = [];
+    $numGroups = ceil(count($apprenants) / $groupSize);
+
+    for ($i = 0; $i < $numGroups; $i++) {
+        $group = [];
+
+        // Ajouter des apprenants back dans le groupe
+        $backCount = min(ceil($groupSize / 2), count($apprenantsBack));
+        $group = array_merge($group, array_splice($apprenantsBack, 0, $backCount));
+
+        // Ajouter des apprenants front dans le groupe
+        $frontCount = min($groupSize - count($group), count($apprenantsFront));
+        $group = array_merge($group, array_splice($apprenantsFront, 0, $frontCount));
+
+        // Ajouter le groupe à la liste des groupes
+        $mixedGroup[] = $group;
+    }
+
+    // Ajouter les apprenants restants à un dernier groupe
+    $remainingGroup = array_merge($apprenantsBack, $apprenantsFront);
+    if (!empty($remainingGroup)) {
+        $mixedGroup[] = $remainingGroup;
+    }
+
+    return $mixedGroup;
+}
 
 // Récupérer le nombre maximum d'apprenants par groupe depuis les paramètres de la requête
 $maxApprenantsPerGroup = isset($_GET['maxApprenantsPerGroup']) ? intval($_GET['maxApprenantsPerGroup']) : 0;
+// Récupérer les différents filtres depuis les paramètres de la requête
 $genderFilter = isset($_GET['genderfilter']) ? filter_var($_GET['genderfilter'], FILTER_VALIDATE_BOOLEAN) : false;
+$skillsFilter = isset($_GET['skillsFilter']) ? filter_var($_GET['skillsFilter'], FILTER_VALIDATE_BOOLEAN) : false;
 
 
 // Vérifier la validité du nombre maximum d'apprenants par groupe
@@ -111,9 +161,11 @@ if ($maxApprenantsPerGroup <= 0) {
 $groupSize = min($maxApprenantsPerGroup, count($apprenants));
 
 // Créer les groupes
-$groups = createGroups($apprenants, $groupSize, $genderFilter);
+$groups = createGroups($apprenants, $groupSize, $genderFilter, $skillsFilter);
 
-// Renvoyer les groupes sous forme de données JSON
+// var_dump($groups);
+
+// // Renvoyer les groupes sous forme de données JSON
 header('Content-Type: application/json');
 echo json_encode($groups);
 ?>
